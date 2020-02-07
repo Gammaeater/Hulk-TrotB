34f8dd56386fce744a205aef2b44eca91aa3acc9
Livio <hipzap@gmail.com>
2020-02-07 11:13:45 +0100

    Enemy Implement
    enemy behaviour implement

--- Assets/Enemy/Scripts/Enemy.cs
+++ Assets/Enemy/Scripts/Enemy.cs
@@ -21,7 +21,7 @@ public class Enemy : MonoBehaviour
         {
             Destroy(gameObject);  
         }
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
 
     }
     public void TakeDamage(int damage)
--- Assets/EnemyMovment.cs
+++ Assets/EnemyMovment.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 
 
@@ -17,6 +19,14 @@ public class EnemyMovment : MonoBehaviour
     public GameObject _target;
     private float moveSpeed;
 
+    public float speed;
+    public float distance;
+    public Transform groundDetection;
+    public bool movingRight = true;
+
+
+
+
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -25,9 +35,29 @@ public class EnemyMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spriteRenderer.flipX = playerCharacter.transform.position.x < transform.position.x;
+       // spriteRenderer.flipX = playerCharacter.transform.position.x < transform.position.x;
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
 
+         float horizontalMove = transform.position.x;
         anim.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        int layer_mask = LayerMask.GetMask("Ground");
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, layer_mask);
+        if(groundInfo.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+
+        }
+
     }
 
     public void CheckDistance()
